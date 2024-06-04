5a93cffa6eb329b00c8230ec6a45a8797623a9fa
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-06-05 00:09:26 +0200

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -4,33 +4,10 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (!m.message) return !0;
   if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
-  const user = global.db.data.users[m.sender];
   const bot = global.db.data.settings[this.user.jid] || {};
-//INICIO lineas por Kurt18
-const fechaActual = new Date();
-const fechaActualNum = Date.UTC(
-    fechaActual.getFullYear(),
-    fechaActual.getMonth(),
-    fechaActual.getDate()
-);
-let q = m.quoted ? m.quoted : m;
-let mime = (q.msg || q).mimetype || q.mediaType || "";
-
-let horaActualNum = Date.now();
-const intervaloEsperado = 1 * 60 * 60 * 1000; // 1 hora en milisegundos
-let tiempoTranscurrido = horaActualNum - user.fechaUltimoMsjInbox;
-
-console.log(`user.fechaUltimoMsjInbox >${user.fechaUltimoMsjInbox}<`); //Luego comentar
-console.log(`horaActualNum >${horaActualNum}<`); //Luego comentar
-console.log(`tiempoTranscurrido >${tiempoTranscurrido}<`); //Luego comentar
-
-if (tiempoTranscurrido >= intervaloEsperado) {
-   await m.reply(`*`🐱Hola @${m.sender.split`@`[0]}, bienvenido al chatbot de MichiBot🐱`*\n> Soy el asistente virtual de Geri, y estare a cargo de atenderte hoy\n\n*En esta ocasion tenemos a la venta:*\n🤖Bot de etiquetas\n📝Bot personalizado\n📨APK de spam\n🎶APK de Spotify premium`, false, {mentions: [m.sender]});
-    user.fechaUltimoMsjInbox = horaActualNum;
-    return;
-}
-//user.fechaUltimoMsjInbox = 0; //test (luego eliminar)
-//FIN lineas por Kurt18
-
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*[❗] Hola @${m.sender.split`@`[0]}, Esta prohibido hablar al privado del bot, te bloqueare`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
   return !1;
-                  }
+}
